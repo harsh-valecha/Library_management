ec1b265a4e896636ffa0cb3e288ba9479be69534
Harsh <harsh.valecha@yahoo.com>
2024-08-16 21:38:39 +0530

    class method and borrow and returning code updated

--- Library/student_member.py
+++ Library/student_member.py
@@ -25,8 +25,29 @@ class StudentMember(Member):
     def generate_random_code():
         return random.randrange(10,20)
 
+    @classmethod
+    def get_borrowed_count(cls):
+        return cls.max_books
+
+
+    def student_borrow_books(self,book):
+        if len(self.borrowed_books)>self.max_books:
+            print('Not allowed return some books please')
+
+        else:
+            self.borrow_books(book)
+
+
+    def student_return_books(self,book):
+        if book in self.borrowed_books:
+            self.return_book(book)
+
+        else:
+            print(f'{book.title} was not borrowed')
+
 
 
 s1 = StudentMember('Kamlesh','EC','5th')
 print(s1.get_student_id(),s1.name,s1.get_course(),s1.get_sem())
 print('static method usecase :',s1.generate_random_code())
+print('class method usecase:',s1.get_borrowed_count())
